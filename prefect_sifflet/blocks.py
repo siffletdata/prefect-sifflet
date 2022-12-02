b42b4b70c09addbc23f24ec7398a245d91ae28c5
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2022-12-02 17:02:14 +0100

    Added Sifflet Client along with convenient methods to trigger rule run and retrieve a rule run

--- prefect_sifflet/blocks.py
+++ prefect_sifflet/blocks.py
@@ -1,7 +1,7 @@
 """This is an example blocks module"""
 
 from prefect.blocks.core import Block
-from pydantic import SecretStr
+from pydantic import Field, SecretStr
 
 
 class SiffletCredentials(Block):
@@ -9,6 +9,7 @@ class SiffletCredentials(Block):
     A block that holds a Sifflet API token.
 
     Attributes:
+        tenant (Field): The tenant of the Sifflet deployment
         api_token (SecretStr): The API token to use to interact with Sifflet.
 
     Example:
@@ -22,4 +23,5 @@ class SiffletCredentials(Block):
     _block_type_name = "sifflet"
     # _logo_url = "https://path/to/logo.png"
 
+    tenant: str = Field("Sifflet tenant")
     api_token = SecretStr("The API token value")
---
+++ prefect_sifflet/client.py
@@ -0,0 +1,81 @@
+"""
+TODO
+"""
+from typing import Dict
+
+from requests import Session
+
+from .blocks import SiffletCredentials
+from .exceptions import SiffletException
+
+
+class SiffletClient:
+    """
+    TODO
+    """
+
+    def __init__(self, credentials: SiffletCredentials) -> None:
+        """
+        TODO
+        """
+        self.credentials = credentials
+        self.api_version = "v1"
+        self.api_base_url = (
+            f"https://{credentials.tenant}api.siffletdata.com/api/{self.api_version}"
+        )
+
+    def _get_session(self) -> Session:
+        """
+        TODO
+        """
+        session = Session()
+        session.headers = {
+            "accept": "application/json",
+            "authorization": f"Bearer {self.credentials.api_token.get_secret_value()}",
+        }
+
+        return session
+
+    def _get_trigger_sifflet_run_api_url(self, rule_id: str) -> str:
+        """
+        TODO
+        """
+        return f"{self.api_base_url}/rules/{rule_id}/_run"
+
+    def _get_sifflet_rule_runs_api_url(self, rule_id: str) -> str:
+        """
+        TODO
+        """
+        return f"{self.api_base_url}/rules/{rule_id}/runs"
+
+    def trigger_sifflet_rule(self, rule_id: str) -> Dict:
+        """
+        TODO
+        """
+        url = self._get_trigger_sifflet_run_api_url(rule_id=rule_id)
+        session = self._get_session()
+
+        with session.get(url=url) as response:
+            if response.status_code != 200:
+                raise SiffletException(response.text)
+            else:
+                return response.json()
+
+    def get_sifflet_rule_run(self, rule_id: str, rule_run_id: str) -> Dict:
+        """
+        TODO
+        """
+        url = self._get_sifflet_rule_runs_api_url(rule_id=rule_id)
+        session = self._get_session()
+
+        with session.get(url=url) as response:
+            if response.status_code != 200:
+                raise SiffletException(response.text)
+
+            rule_runs = response.json()["data"]
+            for rule_run in rule_runs:
+                if rule_run["id"] == rule_run_id:
+                    return rule_run
+
+            err_msg = f"Sifflet rule run not found! Rule ID: {rule_id}, Rule Run ID: {rule_run_id}"  # noqa
+            raise SiffletException(err_msg)
