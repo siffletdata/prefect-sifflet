0229c022050e17f28f39c43fab08756ee87773b8
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2022-12-05 13:27:31 +0100

    Added pagination handling

--- prefect_sifflet/sifflet_client.py
+++ prefect_sifflet/sifflet_client.py
@@ -42,11 +42,19 @@ class SiffletClient:
         """
         return f"{self.api_base_url}/rules/{rule_id}/_run"
 
-    def _get_sifflet_rule_runs_api_url(self, rule_id: str) -> str:
+    def _get_sifflet_rule_runs_api_url(
+        self,
+        rule_id: str,
+        page_id: int,
+        items_per_page: int,
+        sort_key: str,
+        sort_desc: bool = True,
+    ) -> str:
         """
         TODO
         """
-        return f"{self.api_base_url}/rules/{rule_id}/runs"
+        sort_direction = "DESC" if sort_desc else "ASC"
+        return f"{self.api_base_url}/rules/{rule_id}/runs?page={page_id}&itemsPerPage={items_per_page}&sort={sort_key}%2C{sort_direction}"  # noqa
 
     def trigger_sifflet_rule_run(self, rule_id: str) -> Dict:
         """
@@ -67,19 +75,34 @@ class SiffletClient:
         """
         TODO
         """
-        url = self._get_sifflet_rule_runs_api_url(rule_id=rule_id)
-        session = self._get_session()
+        current_page_id = 0
+        items_per_page = 10
 
-        with session.get(url=url) as response:
-            if response.status_code != 200:
-                raise SiffletException(
-                    f"Error while retrieving rule run: {response.text}"
-                )
+        while True:
+            url = url = self._get_sifflet_rule_runs_api_url(
+                rule_id=rule_id,
+                page_id=current_page_id,
+                items_per_page=items_per_page,
+                sort_key="creatDate",
+            )
+            session = self._get_session()
+            with session.get(url=url) as response:
+                if response.status_code != 200:
+                    raise SiffletException(
+                        f"Error while retrieving rule run: {response.text}"
+                    )
 
-            rule_runs = response.json()["data"]
-            for rule_run in rule_runs:
-                if rule_run["id"] == rule_run_id:
-                    return rule_run
+                r = response.json()
+                rule_runs = r["data"]
+                total_runs = r["totalElements"]
+                for rule_run in rule_runs:
+                    if rule_run["id"] == rule_run_id:
+                        return rule_run
 
-            err_msg = f"Sifflet rule run not found! Rule ID: {rule_id}, Rule Run ID: {rule_run_id}"  # noqa
-            raise SiffletException(err_msg)
+                if total_runs // items_per_page > current_page_id:
+                    current_page_id += 1
+                else:
+                    break
+
+        err_msg = f"Sifflet rule run not found! Rule ID: {rule_id}, Rule Run ID: {rule_run_id}"  # noqa
+        raise SiffletException(err_msg)
